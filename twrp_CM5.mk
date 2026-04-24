057c3c166af9f4b2cfa855d7584154be0f6ec759
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-04-24 20:12:45 +0100

    Update twrp_CM5.mk

--- twrp_CM5.mk
+++ twrp_CM5.mk
@@ -4,15 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from TECNO-CM5 device
-$(call inherit-product, device/tecno/TECNO-CM5/device.mk)
+# Inherit from CM5 device
+$(call inherit-product, device/tecno/CM5/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
 PRODUCT_NAME := twrp_CM5
-PRODUCT_DEVICE := TECNO-CM5
+PRODUCT_DEVICE := CM5
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO CM5
 PRODUCT_MANUFACTURER := TECNO
