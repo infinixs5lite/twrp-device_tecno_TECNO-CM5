9f275467c33c689878b0a43681ff24ebd328f4b6
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-04-24 20:42:00 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/transsion/mt6789-common
+DEVICE_PATH := device/tecno/CM5
 
 # Inherit from mt6789-common
 include device/transsion/mt6789-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := TECNO-CM5
+TARGET_OTA_ASSERT_DEVICE := CM5
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_TECNO-CM5
